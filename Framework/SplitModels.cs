e4fec052377edfbd06a682183802061dfda7cfbf
runxc1 <runxc1@gmail.com>
2024-03-16 16:13:16 -0600

    Adding bigger buttons and a history page
    Afer the first track meet of the season and first time using it, it was very evident that the buttons were too small.   Also changed the default number of timers to 4.  It's pretty hard to time more than that many people at once.

---
+++ Framework/SplitModels.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+namespace TrackBlazor.Framework
+{
+    public class Timer
+    {
+        public Timer(string name)
+        {
+            this.Name = name;
+        }
+        public bool editMode { get; set; } = false;
+        public string Name { get; set; }
+        public Stopwatch Stopwatch { get; set; } = new Stopwatch();
+        public List<Split> Splits { get; set; } = new List<Split>();
+        public bool StartedOnce { get; set; } = false;
+        public void Start()
+        {
+            this.Stopwatch.Start();
+            this.StartedOnce = true;
+        }
+        public void Stop()
+        {
+            if (Stopwatch.IsRunning)
+                Stopwatch.Stop();
+        }
+        public void TakeSplit()
+        {
+            var ts = this.Stopwatch.Elapsed;
+            var last = this.Splits.LastOrDefault();
+            this.Splits.Add(new Split(ts, last?.Ellapsed));
+        }
+        public void Reset()
+        {
+            this.Stopwatch.Reset();
+            this.Splits.Clear();
+        }
+        public TimeSpan ElapsedSinceLast
+        {
+            get
+            {
+                if (this.Splits.Any())
+                {
+                    return this.Stopwatch.Elapsed - this.Splits.Last().Ellapsed;
+                }
+                else
+                {
+                    return this.Stopwatch.Elapsed;
+                }
+            }
+        }
+    }
+    public class Split
+    {
+        public TimeSpan Ellapsed { get; set; }
+        public TimeSpan SplitTime { get; set; }
+
+        public Split(TimeSpan ts, TimeSpan? previous)
+        {
+            this.Ellapsed = ts;
+            if (previous.HasValue)
+                this.SplitTime = ts - previous.Value;
+            else
+                this.SplitTime = ts;
+        }
+    }
+    public class SplitResults
+    {
+        public DateTime SaveTime { get; set; }
+        public List<SplitResult> Results { get; set; } = new List<SplitResult>();
+    }
+    public class SplitResult
+    {
+        public string? Name { get; set; }
+        public int Number { get; set; }
+        public TimeSpan Ellapsed { get; set; }
+        public TimeSpan SplitTime { get; set; }
+    }
+}
